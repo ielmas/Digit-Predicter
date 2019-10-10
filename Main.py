ea1813c4223d35cd3b6abb189210c052d62fac91
ibrahim elmas <ibrahimelmas.3452@gmail.com>
2019-10-10 11:31:16 +0300

    cost function and sigmoid function is fully implemented

--- Main.py
+++ Main.py
@@ -5,12 +5,22 @@ Description: Main class that uses both Trainer and Predicter class for
 debugging.
 @author: OrduLou
 """
-
+import numpy as np
 from Trainer import Trainer
 
 class main:
     
     path = 'sampleDatabase.mat'
+    dataset = sio.loadmat(path)
+    X = dataset.contents['X']
+    y = dataset.contents['y']
+    a =(np.arange(9) +1).reshape((3,3))
+    c = np.array([[-2], [-2], [-2]])
+    b = np.array([[2], [2], [2]])
+    print(np.sum(a, 0))
+#    print(np.multiply(1-b,np.log(1-a)))
+#    print(a)
     trainer = Trainer(path)
-    trainer.loadDataset()
-    trainer.costFunction()
+#    trainer.loadDataset()
+#    trainer.costFunction()
+    
--- Trainer.py
+++ Trainer.py
@@ -15,19 +15,30 @@ class Trainer:
     def loadDataset(self):
         self.dataset = sio.loadmat(self.path)
         
-
-    def costFunction(X, y):
+    def Sigmoid(self, h):
+        return 1 / (1 + np.exp(-h))
+        
+    def costFunction(X, y, theta):
         numrow = X.shape[0]         #number of rows
         ones = np.ones(numrow)
         X = np.c_[ones, X]          #now we have correct X matrix
         length = X.shape[1]         #number of columns
-        init_theta = np.zeros(length) 
+        hypthesis = X.dot(theta)
+        hypthesis = Sigmoid(hypthesis)
+        term1 = np.multiply(y, log(hypthesis))
+        term2 = np.multiply(1-y , log(1-hypthesis))
+        term1 = np.sum(term1)
+        term2 = np.sum(term2)
+        return term1 + term2
         
-    def Gradient():
+#    def Gradient():
         
 
     def printDataset(self):
         print(self.dataset)
         print(sorted(self.dataset.keys()))
+    
         
-    def Train():
+#    def Train():
+        
+        
